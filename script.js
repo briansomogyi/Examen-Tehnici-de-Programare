6dbc645cdfe93bb28946c80557da7edbf7f518a5
briansomogyi <brian.somogyi@emanuel.ro>
2024-06-17 09:24:19 +0300

    In sfarsit imi merge canvas-ul

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 let squares;
+let input;
+let button;
 
 function setup() {
-    createCanvas(1250, 600);
-    background(220);
-    squares=new Squares(0,0,100,4);
-  }
-  
-  function draw() {
-    squares.drawSquares();
-  }
+    createCanvas(1920, 1080);
+    input = createInput();
+    input.position(20, height / 2);
+
+    button = createButton("Generează");
+    button.position(input.x + input.width, height / 2);
+    button.mousePressed(generate);
+
+
+}
+
+function draw() {
+
+}
+
+function generate() {
+    let value = input.value();
+    let n = Number(value);
+    input.value('');
+    clear();
+    if (n >= 4 && n % 2 == 0) {
+        squares = new Squares(0, 0, 100, n);
+        squares.drawSquares();
+    } else {
+        console.log("Valoarea lui n este >= 4 si trebuie sa fie para.");
+    }
+}
